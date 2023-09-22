546d672b6c082240091a3d1d6073f2bfa7b88f3b
Nils Hjelte <nils.hjelte@gmail.com>
2023-09-22 14:01:52 +0200

    Add tokens for generic clause and conformance

--- Sources/hylo-lsp/AST+SemanticTokenWalker.swift
+++ Sources/hylo-lsp/AST+SemanticTokenWalker.swift
@@ -18,9 +18,12 @@ extension AST {
     // var outermostFunctions: [FunctionDecl.ID] = []
     private(set) var tokens: [SemanticToken]
     private let document: DocumentUri
+    private let program: TypedProgram
+    private var binding: BindingDecl?
 
-    public init(_ document: DocumentUri) {
+    public init(_ document: DocumentUri, _ program: TypedProgram) {
       self.document = document
+      self.program = program
       self.tokens = []
     }
 
@@ -42,35 +45,122 @@ extension AST {
       }
 
       switch node {
+      case let d as BindingDecl:
+        binding = d
+        return true
       case _ as NamespaceDecl:
         return true
-      case let f as FunctionDecl:
-        tokens.append(SemanticToken(range: f.identifier!.site, type: TokenType.function.rawValue, modifiers: 0))
+      case let d as InitializerDecl:
+        for _ in d.attributes {
+          // TODO
+        }
+
+        tokens.append(SemanticToken(range: d.accessModifier.site, type: TokenType.keyword.rawValue, modifiers: 0))
+
+        tokens.append(SemanticToken(range: d.introducer.site, type: TokenType.function.rawValue, modifiers: 0))
+        addGenericClause(d.genericClause, in: ast)
         return true
-      case let v as VarDecl:
-        tokens.append(SemanticToken(range: v.identifier.site, type: TokenType.variable.rawValue, modifiers: 0))
+      case let d as FunctionDecl:
+
+        for _ in d.attributes {
+          // TODO
+        }
+
+        tokens.append(SemanticToken(range: d.accessModifier.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        if let s = d.memberModifier {
+          tokens.append(SemanticToken(range: s.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        }
+        if let s = d.notation {
+          tokens.append(SemanticToken(range: s.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        }
+        tokens.append(SemanticToken(range: d.introducerSite, type: TokenType.keyword.rawValue, modifiers: 0))
+        if let identifier = d.identifier {
+          tokens.append(SemanticToken(range: identifier.site, type: TokenType.function.rawValue, modifiers: 0))
+        }
+        addGenericClause(d.genericClause, in: ast)
+
+        if let s = d.receiverEffect {
+          tokens.append(SemanticToken(range: s.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        }
+
         return true
-      case let t as AssociatedTypeDecl:
-        tokens.append(SemanticToken(range: t.identifier.site, type: TokenType.type.rawValue, modifiers: 0))
+      case let d as VarDecl:
+        guard let binding = binding else {
+          return true
+        }
+
+        for _ in binding.attributes {
+          // TODO
+        }
+
+
+        tokens.append(SemanticToken(range: binding.accessModifier.site, type: TokenType.keyword.rawValue, modifiers: 0))
+
+        if let s = binding.memberModifier {
+          tokens.append(SemanticToken(range: s.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        }
+
+        tokens.append(SemanticToken(range: d.identifier.site, type: TokenType.variable.rawValue, modifiers: 0))
+        self.binding = nil
         return true
-      case let t as ProductTypeDecl:
-        tokens.append(SemanticToken(range: t.identifier.site, type: TokenType.type.rawValue, modifiers: 0))
+      case let d as AssociatedTypeDecl:
+        tokens.append(SemanticToken(range: d.introducerSite, type: TokenType.keyword.rawValue, modifiers: 0))
+        tokens.append(SemanticToken(range: d.identifier.site, type: TokenType.type.rawValue, modifiers: 0))
         return true
-      case let c as ConformanceDecl:
-        let s = ast[c.subject]
-        tokens.append(SemanticToken(range: s.site, type: TokenType.identifier.rawValue, modifiers: 0))
+      case let d as ProductTypeDecl:
+        tokens.append(SemanticToken(range: d.accessModifier.site, type: TokenType.keyword.rawValue, modifiers: 0))
+        tokens.append(SemanticToken(range: d.identifier.site, type: TokenType.type.rawValue, modifiers: 0))
+        addGenericClause(d.genericClause, in: ast)
+        addConformances(d.conformances, in: ast)
+        return true
+      case let d as ConformanceDecl:
+        let s = ast[d.subject]
+        logger.debug("conformance: \(d)")
+        // tokens.append(SemanticToken(range: d.site, type: TokenType.identifier.rawValue, modifiers: 0))
         return true
       default:
         return true
       }
     }
+
+    mutating func addConformances(_ conformances: [NameExpr.ID], in ast: AST) {
+      for id in conformances {
+        let n = ast[id]
+        tokens.append(SemanticToken(range: n.site, type: TokenType.type.rawValue, modifiers: 0))
+      }
+    }
+
+    mutating func addGenericClause(_ genericClause: SourceRepresentable<GenericClause>?, in ast: AST) {
+      if let genericClause = genericClause {
+        addGenericClause(genericClause.value, in: ast)
+      }
+    }
+
+
+    mutating func addGenericClause(_ genericClause: GenericClause, in ast: AST) {
+      if let s = genericClause.whereClause {
+        tokens.append(SemanticToken(range: s.site, type: TokenType.keyword.rawValue, modifiers: 0))
+      }
+
+      for id in genericClause.parameters {
+        let p = ast[id]
+        tokens.append(SemanticToken(range: p.identifier.site, type: TokenType.type.rawValue, modifiers: 0))
+        addConformances(p.conformances, in: ast)
+
+        if let id = p.defaultValue {
+          let defaultValue = ast[id]
+          tokens.append(SemanticToken(range: defaultValue.site, type: TokenType.type.rawValue, modifiers: 0))
+        }
+      }
+    }
   }
 
-  public func getSematicTokens(_ document: DocumentUri) -> [SemanticToken] {
+  public func getSematicTokens(_ document: DocumentUri, _ program: TypedProgram) -> [SemanticToken] {
     logger.debug("List symbols in document: \(document)")
-    var finder = SemanticTokenWalker(document)
+    var finder = SemanticTokenWalker(document, program)
 
     for m in modules {
+      // logger.debug("Walk module: \(m)")
       walk(m, notifying: &finder)
     }
 
